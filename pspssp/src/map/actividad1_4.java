b809e5d9e1df6e5301865fb27f7979ede17df6a4
mariioalcaiide4 <mario.alcaideperez@iesvalleinclan.es>
2024-10-03 12:23:51 +0200

    Mi primera chamba

---
+++ pspssp/src/map/actividad1_4.java
@@ -0,0 +1,33 @@
+package map;
+
+import java.io.IOException;
+
+public class actividad1_4 {
+    public static void main(String[] args) throws IOException {
+
+    if (args.length <= 0){
+        System.out.println("Proporcione un nombre, por favor");
+        System.exit(-1);
+    }
+
+        System.out.println(args[0]);
+        System.exit(1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ pspssp/src/map/actividad1_6.java
@@ -0,0 +1,21 @@
+package map;
+
+public class actividad1_6 {
+
+    public static void main(String [] args){
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
