58317341512b156dee2a5facc6103aa7f8a6eac5
mariioalcaiide4 <mario.alcaideperez@iesvalleinclan.es>
2024-10-17 14:41:18 +0200

    Brr

---
+++ pspssp_2_1/src/Actividad2/Main.java
@@ -0,0 +1,20 @@
+package Actividad2;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Main implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Hola mundo" + "ID:" + Thread.currentThread().getId());
+
+    }
+
+    public static void main (String[] args) {
+
+
+
+
+
+    }
+}
