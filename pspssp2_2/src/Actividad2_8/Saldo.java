04c9c6f4c9da9ac5bfa517dd909842771ae6f67e
mariioalcaiide4 <mario.alcaideperez@iesvalleinclan.es>
2024-11-20 09:02:07 +0100

    Commit de miercoles

--- pspssp2_2/src/Actividad2_8/Saldo.java
+++ pspssp2_2/src/Actividad2_8/Saldo.java
@@ -17,13 +17,32 @@ public class Saldo {
     void restar(int cantidad){saldo_actual = saldo_actual - cantidad;}
     void sumar(int cantidad){saldo_actual = saldo_actual + cantidad;}
 
-    public void sacarDinero(int cantidad){
+    public void sacarDinero(int cant, String nom){
+        if (getSaldo() >= cant){
+            System.out.println(nom + ": SE VA A RETIRAR SALDO (ACTUAL ES: " + getSaldo() + ")");
+            try {
+                    Thread.sleep(500);
+            } catch (InterruptedException ex){
+                ex.printStackTrace();
+            } restar(cant); //resta la cantidad al saldo
 
+            System.out.println("\t" + nom + " retira => " + cant + " ACTUAL(" + getSaldo() + ")");
+        }   else {
+            System.out.println("No se puede sacar dinero, NO HAY SALDO ("+ getSaldo() + ")");
+        }
+        if (getSaldo() < 0){
+            System.out.println("SALDO NEGATIVO => " + getSaldo() + ")");
+        }
     }
 
-    public void meterDinero(){
-
+    public void ingresarDinero(int cant, String nom){
+        System.out.println(nom + ": SE VA A INGRESAR SALDO (ACTUAL ES: " + getSaldo() + ")");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ex){
+            ex.printStackTrace();
+        } sumar(cant); //suma la cantidad al saldo
+        System.out.println("\t" + nom + " ingresa => " + cant + " ACTUAL(" + getSaldo() + ")");
     }
-
 }
 
