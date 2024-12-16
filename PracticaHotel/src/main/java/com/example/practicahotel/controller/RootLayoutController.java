0e2662413efa83f244bd1971b7d527274ae1e51c
mariioalcaiide4 <mario.alcaideperez@iesvalleinclan.es>
2024-12-16 13:24:56 +0100

    Lune de la semana mas innecesaria de la historia

--- PracticaHotel/src/main/java/com/example/practicahotel/controller/RootLayoutController.java
+++ PracticaHotel/src/main/java/com/example/practicahotel/controller/RootLayoutController.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
